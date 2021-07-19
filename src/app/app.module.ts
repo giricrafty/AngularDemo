a8dae6b07ec33e0f70cb8634c5a05ec648b6ce40
giricrafty <76037659+giricrafty@users.noreply.github.com>
2021-07-19 15:22:37 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+//material imports
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    // material import start
+    MatSliderModule,
+    MatCardModule,
+    MatListModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTabsModule
+    // material import end
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
